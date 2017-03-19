609b48f936c8e0b81e69486f3e51a0cac4de6f96
Toon <toonriem@vub.ac.be>
2017-03-19 12:14:32 +0100

    Debugging

--- sphero_blocks.js
+++ sphero_blocks.js
@@ -1,41 +1,10 @@
 (function(ext) {
     var device = null;
-    var connected = false;
-    var Cylon = require('cylon');
-
-    // Cleanup function when the extension is unloaded
-    ext._shutdown = function() {};
-
-    // Status reporting code
-    // Use this to report missing hardware, plugin or unsupported browser
-    ext._getStatus = function() {
-        if(!connected) return {status: 1, msg: 'Device not connected'};
-        return {status: 2, msg: 'Device connected'};
-    }
-
-    ext._deviceConnected = function(dev) {
-            console.log("test1");
-        if (!device) {
-            device = dev;
-            dev.open({ stopBits: 0, bitRate: 38400, ctsFlowControl: 0 }, function() { console.log("test"); });
-            dev.close();
-            // device = Cylon.robot({
-            //             connections: {
-            //                 bluetooth: { adaptor: 'sphero', port: dev.id }
-            //             },
-
-            //             devices: {
-            //                 sphero: { driver: 'sphero' }
-            //             },
-
-            //             work: function(my) {
-            //                 connected = true;
-            //                 console.log("Sphero connected");
-            //             }
-            //          });
-            // device.start();
-        };
-    };
+    var status = false;
+    var SpheroConnection;
+    var SpheroStatus = 0;
+    var SpheroAppID = getRequest().id?getRequest().id:"falgapmgoopapgbocigmjlclilgjgijb"; //unique app ID for Sphero Scratch App
+    //var Cylon = require('cylon');
 
     ext.my_first_block = function() {
         console.log("test");
@@ -49,7 +18,59 @@
         url: 'https://toonr.github.io/Sphero-Blocks/sphero_blocks.js'
     };
 
-    var serial_info = {type: 'serial'};
+    function getRequest() {
+        var url = location.search; 
+        var theRequest = new Object();
+        if (url.indexOf("?") != -1) {
+        var str = url.substr(1);
+        if (str.indexOf("&") != -1) {
+            strs = str.split("&");
+            for (var i = 0; i < strs.length; i++) {
+            theRequest[strs[i].split("=")[0]] = unescape(strs[i].split("=")[1]);
+            }
+        } else {
+            theRequest[str.split("=")[0]] = unescape(str.split("=")[1]);
+        }
+        }
+        return theRequest;
+    }
+   
+    ext._getStatus = function() {
+        return {status: SpheroStatus, msg: SpheroStatus==2?'Ready':'Not Ready'};
+    };
+    ext._shutdown = function() {
+        //if(poller) poller = clearInterval(poller);
+        status = false;
+    }
+    function getSpheroAppStatus() {
+        chrome.runtime.sendMessage(SpheroAppID, {message: "STATUS"}, function (response) {
+            if (response === undefined) { //Chrome app not found
+                console.log("Chrome app not found");
+                SpheroStatus = 0;
+                setTimeout(getSpheroAppStatus, 1000);
+            }
+            else if (response.status === false) { //Chrome app says not connected
+                SpheroStatus = 1;
+                setTimeout(getSpheroAppStatus, 1000);
+            }
+            else {// successfully connected
+                if (SpheroStatus !==2) {
+                    console.log("Connected");
+                    SpheroConnection = chrome.runtime.connect(SpheroAppID);
+                    SpheroConnection.onMessage.addListener(onMsgApp);
+                }
+                SpheroStatus = 2;
+                setTimeout(getSpheroAppStatus, 1000);
+            }
+        });
+    };
+    function onMsgApp(msg) {
+        var buffer = msg.buffer;
+        for(var i=0;i<buffer.length;i++){
+            //onParse(buffer[i]);
+        }
+    };
+    getSpheroAppStatus();
     // Register the extension
-    ScratchExtensions.register('Sample extension', descriptor, ext, serial_info);
+    ScratchExtensions.register('Sample extension', descriptor, ext);
 })({});
