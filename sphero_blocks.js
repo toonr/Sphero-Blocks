f7a352995b17e28ad3ad401defdd62ffdb554c8f
Toon <toonriem@vub.ac.be>
2017-04-23 02:58:22 +0200

    Debugging

--- sphero_blocks.js
+++ sphero_blocks.js
@@ -135,23 +135,6 @@
         menus: menus[lang],
         url: 'https://toonr.github.io/Sphero-Blocks/sphero_blocks.js'
     };
-
-    function getRequest() {
-        var url = location.search; 
-        var theRequest = new Object();
-        if (url.indexOf("?") != -1) {
-        var str = url.substr(1);
-        if (str.indexOf("&") != -1) {
-            strs = str.split("&");
-            for (var i = 0; i < strs.length; i++) {
-            theRequest[strs[i].split("=")[0]] = unescape(strs[i].split("=")[1]);
-            }
-        } else {
-            theRequest[str.split("=")[0]] = unescape(str.split("=")[1]);
-        }
-        }
-        return theRequest;
-    }
    
     ext._getStatus = function() {
         var response = {status: SpheroStatus}
